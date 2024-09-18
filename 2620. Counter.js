2ee3d9d9587202678a7e200d9144652ff97d98ee
bhagyalaxmi25 <palaibhagyalaxmi81@gmail.com>
2024-09-18 14:11:20 +0530

    JS code

---
+++ 2620. Counter.js
@@ -0,0 +1,14 @@
+var createCounter = function(n) {
+    
+    return function() {
+        return n++;
+        
+    };
+};
+var counter = createCounter(10);
+console.log(counter());
+console.log(counter());
+console.log(counter());
+console.log(counter());
+console.log(counter());
+console.log(counter());
